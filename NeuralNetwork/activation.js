e8554fef8de88b1bba70d5a3c7d1a7ce9c486c0a
Aman Doshi <amandoshi001@gmail.com>
2022-01-30 10:08:34 +0400

    Updated for Heroku - PHP app

--- NeuralNetwork/activation.js
+++ NeuralNetwork/activation.js
@@ -1,3 +1,15 @@
 function sigmoid(x) {
 	return 1 / (1 + Math.exp(-x));
 }
+
+function tanh(x) {
+	return Math.tanh(x);
+}
+
+function ReLU(x) {
+	return Math.max(0, x);
+}
+
+function inverseCoshSquared(x) {
+	return Math.pow(Math.cosh(x), -2);
+}
